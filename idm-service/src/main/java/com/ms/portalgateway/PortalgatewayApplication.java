129e6e4dca350963bd439b386c2634e691417d08
adeputragea <adeputragea28@gmail.com>
2023-11-21 23:12:08 +0700

    inittiate idm microservice

---
+++ idm-service/src/main/java/com/ms/portalgateway/PortalgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ms.portalgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalgatewayApplication.class, args);
+	}
+
+}
