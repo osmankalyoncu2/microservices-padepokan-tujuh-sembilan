b69d54b8c60ac90fafd02aa8b089bed276c2d6ad
adeputragea <adeputragea28@gmail.com>
2023-11-21 23:45:21 +0700

    update pom file and change name of main class of idm service

--- idm-service/src/main/java/com/ms/idm/IdmApplication.java
+++ idm-service/src/main/java/com/ms/idm/IdmApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortalgatewayApplication {
+public class IdmApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortalgatewayApplication.class, args);
+		SpringApplication.run(IdmApplication.class, args);
 	}
 
 }
