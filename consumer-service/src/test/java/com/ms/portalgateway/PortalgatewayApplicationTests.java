84216318804fa1a80ec715cf712dc442ae7b604e
adeputragea <adeputragea28@gmail.com>
2023-11-21 23:15:05 +0700

    initiate consumer microservice

---
+++ consumer-service/src/test/java/com/ms/portalgateway/PortalgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.portalgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
